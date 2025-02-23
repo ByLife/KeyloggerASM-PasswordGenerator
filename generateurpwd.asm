7d9097629d735e029576411fe8fa4ecf0dd0657f
LMARTIN6 <luc.martin@ynov.com>
2025-02-23 22:30:24 +0100

    generateurmodhardcore

--- generateurpwd.asm
+++ generateurpwd.asm
@@ -1,133 +1,172 @@
 section .data
-    welcome_msg db "Bienvenue dans le générateur de mot de passe by LucMARTIN & Léo HAIDAR B-)", 10
+    welcome_msg db "Bienvenue dans le générateur de mot de passe by LucMARTIN & Léo HAIDAR B-)", 10, 10
     welcome_msg_len equ $ - welcome_msg
 
-    prompt_msg  db "Entrez 'simple' pour générer un mot de passe: "
+    commands_msg db "Commandes disponibles:", 10, \
+                    "  simple  : Génère un mot de passe de 8 caractères (chiffres et lettres)", 10, \
+                    "  medium  : Génère un mot de passe de 10 caractères (chiffres, lettres et caractères speciaux)", 10, \
+                    "  hardcore: Génère un mot de passe de 20 caractères (chiffres, lettres et caractères speciaux)", 10, \
+                    "  exit    : Quitte le programme", 10, 10
+    commands_msg_len equ $ - commands_msg
+
+    prompt_msg  db "Entrez une commande: "
     prompt_msg_len equ $ - prompt_msg
 
     success_msg db "Mot de passe généré !", 10
     success_msg_len equ $ - success_msg
 
+    unknown_cmd_msg db "Commande inconnue !", 10, 10
+    unknown_cmd_msg_len equ $ - unknown_cmd_msg
+
     newline db 10
 
-    ; Ensemble des caractères possibles (10 chiffres + 26 lettres majuscules + 26 lettres minuscules = 62 caractères)
     allowed_chars db "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     allowed_len equ $ - allowed_chars
 
-    ; Chaîne déclencheur
-    trigger_str db "simple"
+    allowed_medium_chars db "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()"
+    allowed_medium_chars_len equ $ - allowed_medium_chars
+
+    exit_str   db "exit"
+    exit_str_len equ $ - exit_str
+
+    simple_str db "simple"
+    simple_str_len equ $ - simple_str
+
+    medium_str db "medium"
+    medium_str_len equ $ - medium_str
+
+    hardcore_str db "hardcore"
+    hardcore_str_len equ $ - hardcore_str
 
 section .bss
-    input_buffer resb 16       ; Espace suffisant pour "simple" + retour chariot
-    password_buffer resb 9       ; 8 caractères + caractère nul (optionnel)
-    seed resq 1                ; Pour stocker la graine du générateur pseudo-aléatoire
+    input_buffer resb 16
+    simple_password_buffer resb 9
+    medium_password_buffer resb 11
+    hardcore_password_buffer resb 21
+    seed resq 1
 
 section .text
     global _start
 
 _start:
-    ; Afficher le message de bienvenue
-    mov rax, 1               ; sys_write
-    mov rdi, 1               ; stdout
+    mov rax, 1
+    mov rdi, 1
     mov rsi, welcome_msg
     mov rdx, welcome_msg_len
     syscall
 
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, commands_msg
+    mov rdx, commands_msg_len
+    syscall
+
 main_loop:
-    ; Afficher le prompt (sans saut de ligne pour que la saisie apparaisse sur la même ligne)
     mov rax, 1
     mov rdi, 1
     mov rsi, prompt_msg
     mov rdx, prompt_msg_len
     syscall
 
-    ; Lire l'entrée utilisateur
-    mov rax, 0               ; sys_read
-    mov rdi, 0               ; stdin
+    mov rax, 0
+    mov rdi, 0
     mov rsi, input_buffer
     mov rdx, 16
     syscall
+    mov rbx, rax
+    mov rdi, input_buffer
+trim_loop:
+    cmp rbx, 0
+    je after_trim
+    cmp byte [rdi], 10
+    jne not_newline
+    mov byte [rdi], 0
+not_newline:
+    inc rdi
+    dec rbx
+    jmp trim_loop
+after_trim:
 
-    ; Pour s'assurer d'un rendu propre, afficher un saut de ligne après la saisie
+    mov rdi, input_buffer
+    mov rsi, exit_str
+    mov rcx, exit_str_len
+    repe cmpsb
+    cmp rcx, 0
+    je exit_program
+
+    mov rdi, input_buffer
+    mov rsi, simple_str
+    mov rcx, simple_str_len
+    repe cmpsb
+    cmp rcx, 0
+    je gen_simple
+
+    mov rdi, input_buffer
+    mov rsi, medium_str
+    mov rcx, medium_str_len
+    repe cmpsb
+    cmp rcx, 0
+    je gen_medium
+
+    mov rdi, input_buffer
+    mov rsi, hardcore_str
+    mov rcx, hardcore_str_len
+    repe cmpsb
+    cmp rcx, 0
+    je gen_hardcore
+
     mov rax, 1
     mov rdi, 1
-    mov rsi, newline
-    mov rdx, 1
+    mov rsi, unknown_cmd_msg
+    mov rdx, unknown_cmd_msg_len
     syscall
+    jmp main_loop
 
-    ; Comparer les 6 premiers caractères avec "simple"
-    mov rdi, input_buffer
-    mov rsi, trigger_str
-    mov rcx, 6
-    repe cmpsb
-    jne main_loop           ; Si différent, on recommence
-
-    ; Initialiser la graine avec le compteur du processeur (RDTSC)
-    rdtsc                   ; RAX = partie basse, RDX = partie haute
+gen_simple:
+    rdtsc
     mov rbx, rdx
     shl rbx, 32
     or rbx, rax
     mov [seed], rbx
-
-    ; Générer le mot de passe de 8 caractères
-    mov rcx, 8              ; Compteur pour 8 itérations
-    mov rsi, password_buffer ; Pointeur vers le buffer du mot de passe
-gen_loop:
-    ; seed = seed * 1103515245 + 12345 (même formule que le C rand)
+    mov rcx, 8
+    mov rsi, simple_password_buffer
+simple_loop:
     mov rax, [seed]
     mov rbx, 1103515245
-    mul rbx               ; rdx:rax = rax * rbx
+    mul rbx
     add rax, 12345
     mov [seed], rax
-
-    ; Décalage de 16 bits et réduction modulo 62 pour obtenir un index
     shr rax, 16
-    mov rbx, 62
+    mov rbx, allowed_len
     xor rdx, rdx
-    div rbx               ; RAX = quotient, RDX = reste (index dans [0, 61])
-
-    ; Récupérer le caractère correspondant dans allowed_chars
+    div rbx
     mov rbx, allowed_chars
     add rbx, rdx
     mov dl, byte [rbx]
-
-    ; Stocker le caractère dans le buffer du mot de passe
     mov byte [rsi], dl
     inc rsi
-    loop gen_loop
-
-    ; Terminer la chaîne (optionnel)
+    loop simple_loop
     mov byte [rsi], 0
-
-    ; Afficher un saut de ligne pour espacer l'affichage
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
     mov rdx, 1
     syscall
-
-    ; Afficher le message de succès
     mov rax, 1
     mov rdi, 1
     mov rsi, success_msg
     mov rdx, success_msg_len
     syscall
-
-    ; Afficher un saut de ligne pour espacer
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
     mov rdx, 1
     syscall
-
-    ; Afficher le mot de passe généré (8 caractères)
     mov rax, 1
     mov rdi, 1
-    mov rsi, password_buffer
+    mov rsi, simple_password_buffer
     mov rdx, 8
     syscall
-
-    ; Afficher deux sauts de ligne après le mot de passe
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
@@ -138,5 +177,123 @@ gen_loop:
     mov rsi, newline
     mov rdx, 1
     syscall
+    jmp main_loop
 
-    jmp main_loop         ; Revenir au prompt pour une nouvelle génération
+gen_medium:
+    rdtsc
+    mov rbx, rdx
+    shl rbx, 32
+    or rbx, rax
+    mov [seed], rbx
+    mov rcx, 10
+    mov rsi, medium_password_buffer
+medium_loop:
+    mov rax, [seed]
+    mov rbx, 1103515245
+    mul rbx
+    add rax, 12345
+    mov [seed], rax
+    shr rax, 16
+    mov rbx, allowed_medium_chars_len
+    xor rdx, rdx
+    div rbx
+    mov rbx, allowed_medium_chars
+    add rbx, rdx
+    mov dl, byte [rbx]
+    mov byte [rsi], dl
+    inc rsi
+    loop medium_loop
+    mov byte [rsi], 0
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, success_msg
+    mov rdx, success_msg_len
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, medium_password_buffer
+    mov rdx, 10
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+    jmp main_loop
+
+gen_hardcore:
+    rdtsc
+    mov rbx, rdx
+    shl rbx, 32
+    or rbx, rax
+    mov [seed], rbx
+    mov rcx, 20
+    mov rsi, hardcore_password_buffer
+hardcore_loop:
+    mov rax, [seed]
+    mov rbx, 1103515245
+    mul rbx
+    add rax, 12345
+    mov [seed], rax
+    shr rax, 16
+    mov rbx, allowed_medium_chars_len
+    xor rdx, rdx
+    div rbx
+    mov rbx, allowed_medium_chars
+    add rbx, rdx
+    mov dl, byte [rbx]
+    mov byte [rsi], dl
+    inc rsi
+    loop hardcore_loop
+    mov byte [rsi], 0
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, success_msg
+    mov rdx, success_msg_len
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, hardcore_password_buffer
+    mov rdx, 20
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+    jmp main_loop
+
+exit_program:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
