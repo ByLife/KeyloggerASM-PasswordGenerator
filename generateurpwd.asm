3572ee2ae0f6fe959da2bb38b4e3b20a9bc9d4f1
LMARTIN6 <luc.martin@ynov.com>
2025-02-23 19:48:21 +0100

    ameliorationsgenerateur

--- generateurpwd.asm
+++ generateurpwd.asm
@@ -1,8 +1,8 @@
 section .data
-    welcome_msg db "Bienvenue dans le générateur de mot de passe", 10
+    welcome_msg db "Bienvenue dans le générateur de mot de passe by LucMARTIN & Léo HAIDAR B-)", 10
     welcome_msg_len equ $ - welcome_msg
 
-    prompt_msg  db "Entrez 1 pour générer un mot de passe: "
+    prompt_msg  db "Entrez 'simple' pour générer un mot de passe: "
     prompt_msg_len equ $ - prompt_msg
 
     success_msg db "Mot de passe généré !", 10
@@ -14,24 +14,27 @@ section .data
     allowed_chars db "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     allowed_len equ $ - allowed_chars
 
+    ; Chaîne déclencheur
+    trigger_str db "simple"
+
 section .bss
-    input_buffer resb 2           ; Pour lire la saisie (1 caractère + retour)
-    password_buffer resb 9        ; 8 caractères + caractère nul (optionnel)
-    seed resq 1                   ; Pour stocker la graine du générateur pseudo-aléatoire
+    input_buffer resb 16       ; Espace suffisant pour "simple" + retour chariot
+    password_buffer resb 9       ; 8 caractères + caractère nul (optionnel)
+    seed resq 1                ; Pour stocker la graine du générateur pseudo-aléatoire
 
 section .text
     global _start
 
 _start:
-    ; Afficher le message de bienvenue une seule fois
-    mov rax, 1                  ; sys_write
-    mov rdi, 1                  ; stdout
+    ; Afficher le message de bienvenue
+    mov rax, 1               ; sys_write
+    mov rdi, 1               ; stdout
     mov rsi, welcome_msg
     mov rdx, welcome_msg_len
     syscall
 
 main_loop:
-    ; Afficher le prompt
+    ; Afficher le prompt (sans saut de ligne pour que la saisie apparaisse sur la même ligne)
     mov rax, 1
     mov rdi, 1
     mov rsi, prompt_msg
@@ -39,41 +42,49 @@ main_loop:
     syscall
 
     ; Lire l'entrée utilisateur
-    mov rax, 0                  ; sys_read
-    mov rdi, 0                  ; stdin
+    mov rax, 0               ; sys_read
+    mov rdi, 0               ; stdin
     mov rsi, input_buffer
-    mov rdx, 2
+    mov rdx, 16
     syscall
 
-    ; Vérifier si l'entrée est "1"
-    mov al, byte [input_buffer]
-    cmp al, '1'
-    jne main_loop              ; Sinon, on recommence
+    ; Pour s'assurer d'un rendu propre, afficher un saut de ligne après la saisie
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    ; Comparer les 6 premiers caractères avec "simple"
+    mov rdi, input_buffer
+    mov rsi, trigger_str
+    mov rcx, 6
+    repe cmpsb
+    jne main_loop           ; Si différent, on recommence
 
     ; Initialiser la graine avec le compteur du processeur (RDTSC)
-    rdtsc                       ; RAX = partie basse, RDX = partie haute
+    rdtsc                   ; RAX = partie basse, RDX = partie haute
     mov rbx, rdx
     shl rbx, 32
     or rbx, rax
     mov [seed], rbx
 
     ; Générer le mot de passe de 8 caractères
-    mov rcx, 8                  ; Compteur pour 8 itérations
-    mov rsi, password_buffer    ; Pointeur vers le buffer du mot de passe
+    mov rcx, 8              ; Compteur pour 8 itérations
+    mov rsi, password_buffer ; Pointeur vers le buffer du mot de passe
 gen_loop:
-    ; Calculer : seed = seed * 1103515245 + 12345 (même formule que le C rand)
+    ; seed = seed * 1103515245 + 12345 (même formule que le C rand)
     mov rax, [seed]
     mov rbx, 1103515245
-    mul rbx                     ; rdx:rax = rax * rbx (multiplication non signée)
+    mul rbx               ; rdx:rax = rax * rbx
     add rax, 12345
     mov [seed], rax
 
-    ; Pour obtenir un index dans [0, 61] :
-    ; On décale de 16 bits et on prend le modulo 62
+    ; Décalage de 16 bits et réduction modulo 62 pour obtenir un index
     shr rax, 16
     mov rbx, 62
     xor rdx, rdx
-    div rbx                     ; RAX = quotient, RDX = reste (index dans [0, 61])
+    div rbx               ; RAX = quotient, RDX = reste (index dans [0, 61])
 
     ; Récupérer le caractère correspondant dans allowed_chars
     mov rbx, allowed_chars
@@ -88,11 +99,25 @@ gen_loop:
     ; Terminer la chaîne (optionnel)
     mov byte [rsi], 0
 
-    ; Afficher le message de succès sur une ligne au-dessus
+    ; Afficher un saut de ligne pour espacer l'affichage
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    ; Afficher le message de succès
     mov rax, 1
     mov rdi, 1
     mov rsi, success_msg
     mov rdx, success_msg_len
+    syscall
+
+    ; Afficher un saut de ligne pour espacer
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
     syscall
 
     ; Afficher le mot de passe généré (8 caractères)
@@ -102,13 +127,16 @@ gen_loop:
     mov rdx, 8
     syscall
 
-    ; Afficher un retour à la ligne
+    ; Afficher deux sauts de ligne après le mot de passe
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
     mov rdx, 1
     syscall
 
-    jmp main_loop             ; Revenir au prompt pour une nouvelle génération
-
-; Le programme boucle indéfiniment. Pour l'arrêter, utilisez Ctrl+C.
+    jmp main_loop         ; Revenir au prompt pour une nouvelle génération
