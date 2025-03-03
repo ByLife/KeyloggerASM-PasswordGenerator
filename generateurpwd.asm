2e980a68ae979dd3c8dfbb1a1636a41d80874737
LucMartin91 <105196759+LucMartin91@users.noreply.github.com>
2025-03-03 11:43:03 +0100

    Ajout du stockage des mots de passes dans vault.txt

--- generateurpwd.asm
+++ generateurpwd.asm
@@ -1,4 +1,8 @@
 section .data
+    ; fichier de sauvegarde du vault
+    vault_filename db "vault.txt", 0
+    vault_filename_len equ $ - vault_filename
+
     ; message de bienvenue
     welcome_msg db "Bienvenue dans le générateur de mot de passe by LucMARTIN & Léo HAIDAR B-)", 10, 10
     welcome_msg_len equ $ - welcome_msg
@@ -9,7 +13,7 @@ section .data
                     "  medium   : Génère un mot de passe de 10 caractères (chiffres, lettres et caracteres speciaux)", 10, \
                     "  hardcore : Génère un mot de passe de 20 caractères (chiffres, lettres et caracteres speciaux)", 10, \
                     "  custom   : Génère un mot de passe sur mesure", 10, \
-                    "  vault    : Affiche le vault (mot de passe sauvegardés)", 10, \
+                    "  vault    : Affiche le vault (mots de passe sauvegardés)", 10, \
                     "  help     : Affiche cette aide", 10, \
                     "  exit     : Quitte le programme", 10, 10
     commands_msg_len equ $ - commands_msg
@@ -18,7 +22,7 @@ section .data
     prompt_msg db "Entrez une commande: ", 0
     prompt_msg_len equ $ - prompt_msg
 
-    ; message de succes
+    ; message de succès
     success_msg db "Mot de passe généré !", 10, 0
     success_msg_len equ $ - success_msg
 
@@ -28,11 +32,10 @@ section .data
 
     newline db 10
 
-    ; ensemble de caracteres pour simple
+    ; ensembles de caractères
     allowed_chars db "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 0
     allowed_len equ $ - allowed_chars - 1
 
-    ; ensemble de caracteres pour medium et hardcore
     allowed_medium_chars db "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()", 0
     allowed_medium_chars_len equ $ - allowed_medium_chars - 1
 
@@ -54,7 +57,6 @@ section .data
     help_str db "help", 0
     help_str_len equ $ - help_str - 1
 
-    ; --- Ajouts pour le vault ---
     vault_str db "vault", 0
     vault_str_len equ $ - vault_str - 1
 
@@ -69,7 +71,6 @@ section .data
 
     separator_str db " : ", 0
     separator_str_len equ $ - separator_str
-    ; --- Fin des ajouts pour le vault ---
 
     custom_length_prompt db "Entrez la longueur du mot de passe custom: ", 0
     custom_length_prompt_len equ $ - custom_length_prompt
@@ -125,18 +126,51 @@ section .bss
     custom_password_buffer resb 256
     seed resq 1
 
-    ; --- Variables pour le vault ---
+    ; variables pour le vault en mémoire
     name_input resb 64               ; tampon pour le nom saisi
-    vault_storage resb 1024            ; espace mémoire pour stocker les entrées du vault
+    vault_storage resb 1024            ; zone mémoire pour stocker l'ensemble des entrées
     vault_offset resq 1                ; offset courant dans vault_storage
     last_password_ptr resq 1           ; pointeur sur le dernier mot de passe généré
     last_password_len resq 1           ; longueur du dernier mot de passe généré
-    ; --- Fin des variables pour le vault ---
 
 section .text
     global _start, exit_program, display_result
 
+;-----------------------------------------------------------
+; Routine de chargement du vault depuis le fichier
+;-----------------------------------------------------------
+load_vault:
+    ; ouverture du fichier en lecture seule (syscall 2)
+    mov rax, 2
+    mov rdi, vault_filename
+    mov rsi, 0          ; O_RDONLY
+    syscall
+    cmp rax, 0
+    jl load_vault_end   ; si échec (fichier inexistant), on saute
+    ; rax contient le descripteur de fichier
+    mov r12, rax        ; sauvegarde du FD dans r12
+    ; lecture du contenu dans vault_storage (max 1024 octets)
+    mov rax, 0          ; sys_read
+    mov rdi, r12
+    mov rsi, vault_storage
+    mov rdx, 1024
+    syscall
+    ; nombre d'octets lus dans rax -> on le stocke dans vault_offset
+    mov [vault_offset], rax
+    ; fermeture du fichier (syscall 3)
+    mov rax, 3
+    mov rdi, r12
+    syscall
+load_vault_end:
+    ret
+
+;-----------------------------------------------------------
+; Début du programme
+;-----------------------------------------------------------
 _start:
+    ; chargement du vault depuis le fichier
+    call load_vault
+
     ; affichage du message de bienvenue
     mov rax, 1
     mov rdi, 1
@@ -251,8 +285,10 @@ print_help:
     syscall
     jmp main_loop
 
+;-----------------------------------------------------------
+; Génération des mots de passe
+;-----------------------------------------------------------
 gen_simple:
-    ; génération du mot de passe simple
     rdtsc
     mov rbx, rdx
     shl rbx, 32
@@ -277,10 +313,8 @@ simple_loop:
     inc rsi
     loop simple_loop
     mov byte [rsi], 0
-    ; sauvegarde du pointeur et de la longueur pour le vault
     mov qword [last_password_ptr], simple_password_buffer
     mov qword [last_password_len], 8
-    ; affichage du résultat
     mov rdi, simple_password_buffer
     mov rsi, 8
     call display_result
@@ -288,7 +322,6 @@ simple_loop:
     jmp main_loop
 
 gen_medium:
-    ; génération du mot de passe medium
     rdtsc
     mov rbx, rdx
     shl rbx, 32
@@ -322,7 +355,6 @@ medium_loop:
     jmp main_loop
 
 gen_hardcore:
-    ; génération du mot de passe hardcore
     rdtsc
     mov rbx, rdx
     shl rbx, 32
@@ -356,14 +388,13 @@ hardcore_loop:
     jmp main_loop
 
 gen_custom:
-    ; génération du mot de passe custom
+    ; génération du mot de passe custom (similaire aux précédents)
 custom_length_input:
     mov rax, 1
     mov rdi, 1
     mov rsi, custom_length_prompt
     mov rdx, custom_length_prompt_len
     syscall
-    ; lecture de la longueur custom
     mov rax, 0
     mov rdi, 0
     mov rsi, input_buffer
@@ -382,7 +413,6 @@ custom_length_not_newline:
     dec rbx
     jmp custom_length_trim
 custom_length_conv:
-    ; vérifie si le champ est vide
     cmp byte [input_buffer], 0
     je custom_empty_input
     xor rax, rax
@@ -411,10 +441,7 @@ custom_conv_invalid:
 custom_conv_done:
     mov r9, rax
 
-    ; initialisation du charset custom
     mov r8, custom_charset
-
-    ; demande pour lettres minuscules
 lowercase_loop:
     mov rax, 1
     mov rdi, 1
@@ -451,7 +478,6 @@ lowercase_check:
     repe cmpsb
     cmp rcx, 0
     je set_lowercase_no
-    ; ce commantaire contient une faulte intentionelle
     mov rax, 1
     mov rdi, 1
     mov rsi, invalid_response_msg
@@ -466,7 +492,6 @@ set_lowercase_yes:
     mov r8, rdi
 set_lowercase_no:
 
-    ; demande pour lettres majuscules
 uppercase_loop:
     mov rax, 1
     mov rdi, 1
@@ -517,7 +542,6 @@ set_uppercase_yes:
     mov r8, rdi
 set_uppercase_no:
 
-    ; demande pour nombres
 numbers_loop:
     mov rax, 1
     mov rdi, 1
@@ -568,7 +592,6 @@ set_numbers_yes:
     mov r8, rdi
 set_numbers_no:
 
-    ; demande pour caracteres speciaux
 special_loop:
     mov rax, 1
     mov rdi, 1
@@ -660,7 +683,6 @@ custom_gen_done:
     jmp main_loop
 
 custom_empty_input:
-    ; champ vide pour la longueur custom
     mov rax, 1
     mov rdi, 1
     mov rsi, empty_number_msg
@@ -669,7 +691,6 @@ custom_empty_input:
     jmp custom_length_input
 
 custom_no_charset:
-    ; aucun charset sélectionné
     mov rax, 1
     mov rdi, 1
     mov rsi, none_selected_msg
@@ -683,36 +704,33 @@ exit_program:
     xor rdi, rdi
     syscall
 
-; fonction d'affichage du résultat
-; rdi = pointer to password, rsi = length of password
+;-----------------------------------------------------------
+; Fonction d'affichage du résultat
+; rdi = pointeur du mot de passe, rsi = longueur
+;-----------------------------------------------------------
 display_result:
-    mov r8, rdi   ; sauvegarde du pointer
-    mov r9, rsi   ; sauvegarde de la longueur
-    ; affichage d'un saut de ligne
+    mov r8, rdi
+    mov r9, rsi
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
     mov rdx, 1
     syscall
-    ; affichage du message de succès
     mov rax, 1
     mov rdi, 1
     mov rsi, success_msg
     mov rdx, success_msg_len
     syscall
-    ; affichage d'un saut de ligne
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
     mov rdx, 1
     syscall
-    ; affichage du mot de passe
     mov rax, 1
     mov rdi, 1
     mov rsi, r8
     mov rdx, r9
     syscall
-    ; affichage de deux sauts de ligne
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
@@ -725,12 +743,13 @@ display_result:
     syscall
     ret
 
-; --- Fonctionnalité Vault ---
-; Demande à l'utilisateur s'il veut sauvegarder le mot de passe, et si oui,
-; lui demande un nom et enregistre l'entrée dans vault_storage.
+;-----------------------------------------------------------
+; Fonction Vault : demande si on sauvegarde le mot de passe,
+; ajoute une entrée au vault en mémoire ET dans le fichier
+;-----------------------------------------------------------
 vault_prompt:
-    cld  ; efface le flag de direction pour lodsb/stosb
-    ; affiche le prompt de sauvegarde
+    cld
+    ; affichage du prompt de sauvegarde
     mov rax, 1
     mov rdi, 1
     mov rsi, vault_save_prompt
@@ -744,7 +763,6 @@ vault_prompt:
     mov rdx, 16
     syscall
 
-    ; suppression des sauts de ligne
     mov rbx, rax
     mov rdi, input_buffer
 vault_prompt_trim:
@@ -758,7 +776,6 @@ vault_prompt_not_newline:
     dec rbx
     jmp vault_prompt_trim
 vault_prompt_after_trim:
-
     ; vérifie si la réponse est "oui"
     mov rdi, input_buffer
     mov rsi, yes_str
@@ -767,21 +784,22 @@ vault_prompt_after_trim:
     cmp rcx, 0
     jne vault_prompt_end   ; si ce n'est pas "oui", on ne sauvegarde pas
 
-    ; demande le nom pour le mot de passe
+    ; stocke l'offset actuel (avant ajout) dans r10
+    mov r10, [vault_offset]
+
+    ; demande le nom du mot de passe
     mov rax, 1
     mov rdi, 1
     mov rsi, vault_name_prompt
     mov rdx, vault_name_prompt_len
     syscall
 
-    ; lecture du nom dans name_input
     mov rax, 0
     mov rdi, 0
     mov rsi, name_input
     mov rdx, 64
     syscall
 
-    ; suppression des sauts de ligne dans le nom
     mov rbx, rax
     mov rdi, name_input
 vault_name_trim:
@@ -796,15 +814,14 @@ vault_name_not_newline:
     jmp vault_name_trim
 vault_name_trim_done:
 
-    ; Construction de l'entrée du vault : [nom] " : " [motdepasse] "\n"
-    ; Récupère l'offset courant dans vault_storage
+    ; Construction de l'entrée : [nom] " : " [motdepasse] "\n"
+    ; Le pointeur de destination est calculé à partir de vault_storage + [vault_offset]
     mov rax, [vault_offset]
     mov rdi, vault_storage
     add rdi, rax
 
-    cld  ; s'assurer que DF est à 0
-    ; copie le nom depuis name_input
-    mov rsi, name_input
+    cld
+    ; copie du nom
 vault_copy_name:
     lodsb
     cmp al, 0
@@ -822,7 +839,6 @@ vault_copy_sep_loop:
     jmp vault_copy_sep_loop
 
 vault_copy_password:
-    ; copie le mot de passe depuis last_password_ptr (mot de passe déjà sauvegardé)
     mov rsi, [last_password_ptr]
     mov rcx, [last_password_len]
 vault_copy_pass_loop:
@@ -835,19 +851,64 @@ vault_copy_pass_loop:
     jmp vault_copy_pass_loop
 
 vault_copy_newline:
-    ; ajoute un saut de ligne à la fin de l'entrée
     mov al, 10
     stosb
 
-    ; mise à jour de vault_offset : nouvelle position = rdi - vault_storage
+    ; calcul du nouvel offset dans le vault
+    mov r11, rdi          ; r11 = pointeur final
     mov rax, vault_storage
-    sub rdi, rax
-    mov [vault_offset], rdi
+    sub r11, rax          ; r11 = nouveau offset
+    ; new_entry_length = r11 - r10
+    mov rax, r11
+    sub rax, r10          ; rax = taille de l'entrée ajoutée
+    ; mise à jour de vault_offset
+    mov [vault_offset], r11
+
+    ; Appel de la routine d'écriture dans le fichier pour sauvegarder la nouvelle entrée
+    ; On passe en paramètres :
+    ;   rsi = vault_storage + old_offset (début de la nouvelle entrée)
+    ;   rdx = nouvelle entrée (taille)
+    lea rsi, [vault_storage + r10]
+    mov rdx, rax
+    call vault_save_to_file
 
 vault_prompt_end:
     ret
 
-; Affiche toutes les entrées sauvegardées dans le vault
+;-----------------------------------------------------------
+; Routine d'écriture de la nouvelle entrée dans le fichier vault.txt
+; Ouvre le fichier en mode append et écrit les données passées en rsi (taille en rdx)
+;-----------------------------------------------------------
+vault_save_to_file:
+    ; Sauvegarde le pointeur et la taille dans r12 et r13
+    mov r12, rsi    ; r12 = pointer vers l'entrée à écrire
+    mov r13, rdx    ; r13 = taille de l'entrée
+    ; ouverture du fichier avec : O_WRONLY | O_APPEND | O_CREAT = 1089
+    mov rax, 2
+    mov rdi, vault_filename
+    mov rsi, 1089      ; flags
+    mov rdx, 420       ; mode 0644
+    syscall
+    cmp rax, 0
+    jl vault_save_end
+    mov rbx, rax       ; sauvegarde du FD dans rbx
+    ; restauration des paramètres d'écriture
+    mov rsi, r12
+    mov rdx, r13
+    ; écriture de l'entrée dans le fichier
+    mov rax, 1
+    mov rdi, rbx
+    syscall
+    ; fermeture du fichier
+    mov rax, 3
+    mov rdi, rbx
+    syscall
+vault_save_end:
+    ret
+
+;-----------------------------------------------------------
+; Affiche toutes les entrées sauvegardées dans le vault (en mémoire)
+;-----------------------------------------------------------
 show_vault:
     mov rax, [vault_offset]
     cmp rax, 0
